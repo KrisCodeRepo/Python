d5dd08eb237dba4ce125b7602b5fe3836c737a0d
Krishna K <Krishnac30@gmail.com>
2025-04-14 20:20:17 +0530

    Implement greeting functionality and user input in hello.py; add project configuration files for IDE setup

---
+++ Day1.py
@@ -0,0 +1,24 @@
+# print is used to print the output to the console
+# input is used to take input from the user
+# len is used to find the length of a string
+print('Hello World!!')
+print('Hello World!!\nHello World!!\nHello World!!')
+print('Hello Ammu!!\n' * 3)
+print('Hello' + ' ' +'APPU!!')
+print('Hello' + ' ' +'APPU!!' * 3)
+print('Hello' + input('Enter your name: ') + '!!')
+name = input('Enter your name: ')
+print('Hello ' + name + '!!')
+
+# Variables
+name = 'Jack'
+print(name)
+
+name = 'Danny'
+print(name)
+
+print(len(input("What is your name?")))
+
+username = input("What is your username?")
+length = len(username)
+print(length)
--- hello.py
+++ hello.py
@@ -1,1 +1,4 @@
-print('Hello, World')
+### This is a simple Python program that asks for the user's name and prints a greeting message.
+name = input('What is your name?')
+from1 = input('Where are you from?')
+print(f'Hello,{name}, {from1}')
